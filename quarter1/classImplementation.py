e1fc1cce9641fd74835fd9b63a6737a78d1fc1c8
wayzelll <yalalegre@brc.pshs.edu.ph>
2026-09-08 12:49:48 +0800

    Add OPM class with methods for genre and artist search

--- quarter1/classImplementation.py
+++ quarter1/classImplementation.py
@@ -1,1 +1,22 @@
 
+class OPM:
+  #Original Pop Music
+  def __init__(username, genre, artist, date, listeners):
+    username.attribute1 = genre
+    username.attribute2 = artist
+    username.__private_attribute = date
+    username.__private_attribute = listeners
+
+  def searchGenre(genre):
+    genre_ = str(input("Type your OPM genre"))
+    print(search_genre)
+
+  def searchArtist(artist):
+    artist_ = str(input("Type the artist you like"))
+    print(search_artist)
+
+  def createPlaylist():
+    playlist_ = 
+  
+  
+                  
