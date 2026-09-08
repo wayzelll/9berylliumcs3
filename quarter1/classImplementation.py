24a4ed8f1891d318b81cda092f21d337742450a4
wayzelll <yalalegre@brc.pshs.edu.ph>
2026-09-08 13:06:19 +0800

    Enhance user input prompts and playlist functionality
    Refactor OPM class methods for improved user prompts and playlist creation.

--- quarter1/classImplementation.py
+++ quarter1/classImplementation.py
@@ -1,4 +1,4 @@
-
+def opm()
 class OPM:
   #Original Pop Music
   def __init__(username, genre, artist, date, listeners):
@@ -8,15 +8,20 @@ class OPM:
     username.__private_attribute = listeners
 
   def searchGenre(genre):
-    genre_ = str(input("Type your OPM genre"))
-    print(search_genre)
+    genre_ = str(input("Type your OPM genre: "))
+    print(f"{genre_} is now playing")
 
   def searchArtist(artist):
-    artist_ = str(input("Type the artist you like"))
-    print(search_artist)
+    artist_ = str(input("Type the artist you like: "))
+    print(f"{artist_}'s songs are now playing")
 
   def createPlaylist():
-    playlist_ = 
+    playlist_ = genre_, artist_
+    playlist_ = str(input("Enter your playlist's name"))
+    print(playlist_)
+__init__()
+    
+    
   
   
                   
